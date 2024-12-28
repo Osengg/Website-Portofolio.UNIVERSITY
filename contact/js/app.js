88f7513f0776eb5f87d0e0ef589a3be82d4054d5
Lukma <lukman.nurul2693@gmail.com>
2024-12-28 10:03:14 +0700

    first commit

---
+++ contact/js/app.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add("bg-gray-800", "shadow-lg");
+            navbar.classList.remove("bg-transparent");
+        } else {
+            navbar.classList.add("bg-transparent");
+            navbar.classList.remove("bg-gray-800", "shadow-lg");
+        }
+    });
+
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("menu");
+    menuButton.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 5) {
+        navbar.classList.add('text-white');
+        navbar.classList.remove('text-gray-200');
+    } else {
+        navbar.classList.remove('text-white');
+        navbar.classList.add('text-gray-200');
+    }
+});
+
+function toggleOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.classList.toggle('hidden');
+};
+
+function handleSubmit() {
+    this.submitted = true;
+
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
+    document.getElementById('message').value = '';
+
+    setTimeout(() => {
+        this.submitted = false;
+    }, 3500);
+};
---
+++ home/js/app.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 0) {
+            navbar.classList.add("bg-gray-800", "shadow-lg");
+            navbar.classList.remove("bg-transparent");
+        } else {
+            navbar.classList.add("bg-transparent");
+            navbar.classList.remove("bg-gray-800", "shadow-lg");
+        }
+    });
+
+    const menuButton = document.getElementById("menu-button");
+    const menu = document.getElementById("menu");
+    menuButton.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 5) {
+        navbar.classList.add('text-white');
+        navbar.classList.remove('text-gray-200');
+    } else {
+        navbar.classList.remove('text-white');
+        navbar.classList.add('text-gray-200');
+    }
+});
+
+function toggleOverlay() {
+    const overlay = document.getElementById('overlay');
+    overlay.classList.toggle('hidden');
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const redirectPage = urlParams.get('page');
+document.getElementById('closeButton').addEventListener('click', function() {
+    setTimeout(function() {
+        if (redirectPage) {
+            window.location.href = redirectPage;
+        } else {
+            window.history.back();
+        }
+    }, 1000);
+});
